87efc3f2c60115e6c90c61d3e1a185677fd6b4af
negisan <juchuang123@gmail.com>
2026-02-12 13:41:12 +0900

    [fix]seqlize設定ファイルの追加

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -3,10 +3,10 @@ const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOSTNAME } = process.env
 
 module.exports = {
   development: {
-    username: 'root',
-    password: null,
-    database: 'database_development',
-    host: '127.0.0.1',
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOSTNAME,
     dialect: 'mysql',
   },
   test: {
